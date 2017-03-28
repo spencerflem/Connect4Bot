0a1296b374ef3b09fa6f8ee103d9ed95422262cd
Spencer <spencerflem@gmail.com>
2017-03-28 15:53:27 -0400

    For some reason this didnt take last time

--- Vision.h
+++ Vision.h
@@ -1,13 +1,14 @@
 #ifndef VISION_H
 #define VISION_H
 
+#include "Input.h"
 #include "Move.h"
-//#include "opencv2/opencv.hpp"
+#include <opencv2/opencv.hpp>
 
-class Vision {
+class Vision: public Input {
 public:
 	Vision();
-	Move getMove();
+	Move getMove(int player);
 };
 
 #endif
