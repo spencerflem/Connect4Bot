19e696eb16d45ac6d0e8dd22d3afec6cd3d0a757
Armando Robles <arobles.us@gmail.com>
2017-04-15 21:11:54 -0400

    Update AI.cpp

--- AI.cpp
+++ AI.cpp
@@ -26,28 +26,7 @@ Move AI::makeMove(GameState gameState, int player) {
 		col = rand() % 7;
 	} while (availableColumns[col] != 3);
 	return Move(player, col);
-	//TODO getOptions and make a moveconsidering the options
-	/*
-		simple offensive AI:
-		always pick the position with the most 1's surrounding it.
-
-		makes no attempt at stopping the player from completing a connect 4
-
-		simple defensive AI:
-		always pick the next empty spot in a column with two or more "exposed" 2's,
-		or position in a row with two or more "exposed" 2's, excluding positions in
-		columns 1 and 7.
-
-		has no defense against diagonal connects.
-		does not make offensive moves, thus bound to lose.
-
-		whimsical simple AI:
-		randomly chooses either a simple defensive or simple offensive move.
-
-		effectiveness unknown.
-
-		complex AI: ???read a tutorial???
-	*/
+	
 
 }
 
