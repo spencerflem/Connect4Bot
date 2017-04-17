d8f9a2abb906e61bb9cf6f44fa48c7df78cd0763
Armando Robles <arobles.us@gmail.com>
2017-04-17 01:01:20 -0400

    Some comments
    There were no comments :(

--- Main.cpp
+++ Main.cpp
@@ -28,6 +28,7 @@ int main(int argc, char *argv [])
 	int finished = false;
 	Move turn;
 	Game game;
+	//Determining if the game will be played with webcam and audio cues
 	#ifdef VISION
 		Vision input = Vision();
 	#else
@@ -42,21 +43,25 @@ int main(int argc, char *argv [])
 	int player = 1;
 	while (!finished) {
 		if (player == 1) {
+			//AI is passed the digital version of the game board and determines where the next disk is placed
 			turn = ai.makeMove(game.getGameState(), player);
+			//If vision is available, the AI determines where to move and asks the player to make the move
 			if (vision) {
 				output.requestMove(turn.column + 1);
 				Move playerTurn = input.getMove(game.getGameState(), player);
 				if (playerTurn != turn) {
+					//Checking if the player has cheated by placing the disk incorrectly, or made a mistake
 					output.badMove();
-					return -1; //more graceful way?
+					return -1;
 				}
 			}
 		}
 		else {
+			//The player makes his move
 			turn = input.getMove(game.getGameState(), player);
 			if (turn.player != player) {
 				output.badMove();
-				return -1; //more graceful way?
+				return -1;
 			}
 		}
 		if (game.takeTurn(turn)) {
@@ -68,11 +73,14 @@ int main(int argc, char *argv [])
 				player--;
 			}
 		}
+		//Evaluate whether either the AI or user has won the game
 		finished = game.isWin();
 	}
+	//Print the board after the winning move is made
 	if (finished > 1) {
 		output.printBoard(game.getGameState());
 	}
+	//Announce the disk color for the winning player
 	output.announceWinner(finished);
 
 	return 0;
